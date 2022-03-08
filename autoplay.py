2dfcbf1ca97b9459b7f4f5a5bac95077e8e84c5f
dhruvm4 <dhruvm4@illinois.edu>
2022-03-07 20:01:11 -0600

    Successfully added text to speech Now Playing Feature.

--- autoplay.py
+++ autoplay.py
@@ -2,17 +2,31 @@ import playsound
 import os
 import random
 import math
+import pyttsx3
 
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+engine.setProperty('voices', voices[0].id)
+
+#Speak Function
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
 
 directory = "Songs"
 path = "Songs\\"
 songList = []
 for filename in os.listdir(directory):
-    songName = path + filename
-    songList.append(songName)
+    songList.append(filename)
 
 
 while True:
     i = math.floor(random.random() * len(songList))
     print(songList[i])
-    playsound.playsound(songList[i])
+    speak("Now Playing " + songList[i])
+    songName = path + songList[i]
+    try:
+        playsound.playsound(songName)
+    except:
+        speak("Error in file name. Sorry!")
+    
